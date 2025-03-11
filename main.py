146cc16814730eea241552a8a6ab654c10035e1f
Angel Amaya <135993385+aamaya33@users.noreply.github.com>
2025-03-11 14:53:14 -0400

    shifted from local storage to localdb via sqllite

--- main.py
+++ main.py
@@ -6,41 +6,78 @@
 
 from fastapi import FastAPI, HTTPException
 from models import Book 
+import sqlite3
 
 app = FastAPI() 
 
-books_db = []
+def get_db_connection():
+    conn = sqlite3.connect('books.db')
+    conn.row_factory = sqlite3.Row # Why do we need this? 
+    return conn
+
+conn = get_db_connection()
+cur = conn.cursor()
+cur.execute("""CREATE TABLE IF NOT EXISTS books(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            author TEXT NOT NULL,
+            description TEXT NOT NULL      
+            ) """)
+conn.commit()
+conn.close()
 
 @app.get("/books")
 def index():
-    return books_db
+    con = get_db_connection()
+    cur = con.cursor()
+    cur.execute("SELECT * FROM books")
+    books = cur.fetchall()
+    con.close()
+    return books
+    
 
 @app.get("/books/{book_id}")
 def search(book_id: int):
-    for index, book in enumerate(books_db):
-        if book_id == index: 
-            return book
-    raise HTTPException(404, "Book not found")
+    con = get_db_connection()
+    cur = con.cursor()
+    cur.execute('SELECT * FROM books WHERE id = ?', (book_id,))
+    book = cur.fetchone()
+    con.close()
+    if book:
+        return book
+    raise HTTPException(status_code=404, detail="Book not found")
 
 @app.post("/books")
 def add_book(book: Book):
-    book.id = len(books_db) + 1 
-    books_db.append(book)
+    con = get_db_connection()
+    cur = con.cursor()
+    cur.execute("INSERT INTO books (title, author, description) VALUES (?, ?, ?)", (book.title, book.author, book.description))
+    con.commit()
+    con.close()
     return {"message": "Book added successfully"}
 
 @app.put("/books/{book_id}")
 def update_book(book_id: int, updated_book: Book):
-    for index, book in enumerate(books_db):
-        if book.id == book_id:
-            books_db[index] = updated_book
-            updated_book.id = book_id
-            return updated_book
+    con = get_db_connection()
+    cur = con.cursor()
+    cur.execute('UPDATE books SET title = ?, author = ?, description = ? WHERE id = ?', (updated_book.title, updated_book.author, updated_book.description, book_id))
+    con.commit() 
+    # this is lowkey gonna be some funky logic, but i need to add some error handling 
+    cur.execute('SELECT * FROM books WHERE id = ?', (book_id,))
+    book = cur.fetchone()
+    con.close()
+    if book:
+        return book
     raise HTTPException(status_code=404, detail="Book not found")
-
+    
 
 @app.delete("/books/{book_id}")
 def delete_book(book_id: int):
-    for index, book in enumerate(books_db):
-        if index == book_id:
-            books_db.pop(index)
+    con = get_db_connection()
+    cur = con.cursor()
+    cur.execute('DELETE FROM books WHERE id = ?', (book_id,))
+    con.commit()
+    con.close()
+    if cur.rowcount:
+        return {"message": "Book deleted successfully"}
     return HTTPException(404, "Book not found")
